f6653d703ee6f3e1309aed5ac7051f6e3d998564
may0723 <z5146117@ad.unsw.edu.au>
2018-05-19 20:26:53 +1000

    allegy is working properly

--- model.py
+++ model.py
@@ -18,8 +18,6 @@ def get_data():
     return [ingredient, time, allergy, exclude]
 
 
-
-
 def process_request(ingredient, time, allergy, exclude):
     roles.pageNo = 0
     roles.parameters = {"_app_id": "ae10c158", "_app_key": "b5dd6ea0a5e8ffc8fbf8282a1caf0744", "requiredPictures": "true"}
@@ -30,6 +28,10 @@ def process_request(ingredient, time, allergy, exclude):
         roles.parameters['maxTotalTimeInSeconds'] = time
     if allergy != "null":
         allergy = allergy.split()
+        i = 0
+        while i < len(allergy):
+            allergy[i] = trim_allergy(allergy[i])
+            i = i + 1
         roles.parameters['allowedAllergy'] = allergy
     if exclude != "null":
         exclude = exclude.split()
@@ -51,3 +53,33 @@ def change_picture_size(data):
         data['matches'][i]['smallImageUrls'][0] = "".join(s)
         i = i+1
     return data
+
+def  trim_allergy(allergy):
+    l = list(allergy)
+    l[0] = l[0].upper()
+    allergy = "".join(l)
+    l = []
+    if allergy == "Gluten":
+        l.append("393^")
+    elif allergy == "Peanut":
+        l.append("394^")
+    elif allergy == "Seafood":
+        l.append("398^")
+    elif allergy == "Sesame":
+        l.append("399^")
+    elif allergy == "Soy":
+        l.append("400^")
+    elif allergy == "Dairy":
+        l.append("396^")
+    elif allergy == "Egg":
+        l.append("397^")
+    elif allergy == "Sulfite":
+        l.append("401^")
+    elif allergy == "Tree Nut":
+        l.append("395^")
+    elif allergy == "Wheat":
+        l.append("392^")
+    l.append(allergy)
+    l.append("-Free")
+    allergy = "".join(l)
+    return allergy
